802fdea7a3682151ff72aa37007b1f0b76903fa5
Adrian Lehmann <ownadi@gmail.com>
2018-01-09 14:19:17 +0100

    Limit heartbeats

--- lib/appsignal/heartbeat.rb
+++ lib/appsignal/heartbeat.rb
@@ -1,20 +1,16 @@
+require 'fileutils'
+
 module Appsignal
   class Heartbeat
+    CONTROL_FILE_PATH = File.join(Dir.pwd, 'tmp', 'last_heartbeat')
+
     class << self
       def start
         Appsignal.logger.debug("Starting heartbeat thread")
         Thread.new do
           begin
             loop do
-              Appsignal.monitor_transaction(
-                'perform_job.heartbeat',
-                class: 'Appsignal',
-                method: 'heartbeat',
-                queue_start: Time.now
-              ) do
-                sleep(1)
-              end
-              Appsignal.logger.debug("Heartbeat")
+              do_heartbeat
               sleep(wait_time)
             end
           rescue => ex
@@ -23,8 +19,30 @@ module Appsignal
         end
       end
 
+      def do_heartbeat
+        if right_time?
+          Appsignal.monitor_transaction(
+            'perform_job.heartbeat',
+            class: 'Appsignal',
+            method: 'heartbeat',
+            queue_start: Time.now
+          ) do
+            sleep(1)
+          end
+          Appsignal.logger.debug("Heartbeat")
+          FileUtils.touch(CONTROL_FILE_PATH)
+        end
+      end
+
       def wait_time
         4 * 60 * 60
+      end
+
+      def right_time?
+        last_heartbeat_at = File.mtime(CONTROL_FILE_PATH) rescue nil
+        return true unless last_heartbeat_at
+
+        Time.now - last_heartbeat_at >= wait_time
       end
     end
   end
